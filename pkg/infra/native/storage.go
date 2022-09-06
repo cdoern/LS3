b4c714778139cd0c03a8a9b186ade77e5724e616
Charlie Doern <cdoern@redhat.com>
2022-09-05 21:49:57 -0400

    initial structural commit
    in this commit, I have implemented the command line interface for ls3, and put in the bare bones of the actual ls3 functional interface
    
    Signed-off-by: Charlie Doern <cdoern@redhat.com>

---
+++ pkg/infra/native/storage.go
@@ -0,0 +1,23 @@
+//go:build linux
+
+package native
+
+func CopyObject() {
+
+}
+
+func DeleteObject() {
+
+}
+
+func GetObject() {
+
+}
+
+func ListObject() {
+
+}
+
+func PutObject() {
+
+}
---
+++ pkg/infra/unsupported/storage.go
@@ -0,0 +1,23 @@
+//go:build !linux
+
+package unsupported
+
+func CopyObject() {
+
+}
+
+func DeleteObject() {
+
+}
+
+func GetObject() {
+
+}
+
+func ListObject() {
+
+}
+
+func PutObject() {
+
+}
