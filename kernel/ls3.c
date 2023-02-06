9ebe47baf18ddcd84226414ad53e0e211f5a28b6
Kevin Walsh <kwalsh@holycross.edu>
2023-02-06 01:28:03 +0000

    Fix indentation

--- kernel/ls3.c
+++ kernel/ls3.c
@@ -59,155 +59,155 @@ module_param(verbose, int, 0644);
 MODULE_PARM_DESC(verbose, "Amount of debug printing");
 
 static int write_array(char *key, char *value, uint64_t key_len, uint64_t value_len) {
-        struct appendable_data append;
-        append.key = key;
-        append.value = value;
-        append.key_len = key_len;
-        append.value_len = value_len;
-        int loop;
-        int place = tracker;
-        for(loop = 0; loop < tracker; loop++) {
-            printk(KERN_INFO "key, value: %s, %s\n", all_data[loop].key, all_data[loop].value);
-            if (all_data[loop].key == NULL) {
-                place = loop;
-            } else if (strcmp(all_data[loop].key, key) == 0) {
-                place = loop;
-                break;
-            }
+    struct appendable_data append;
+    append.key = key;
+    append.value = value;
+    append.key_len = key_len;
+    append.value_len = value_len;
+    int loop;
+    int place = tracker;
+    for(loop = 0; loop < tracker; loop++) {
+        printk(KERN_INFO "key, value: %s, %s\n", all_data[loop].key, all_data[loop].value);
+        if (all_data[loop].key == NULL) {
+            place = loop;
+        } else if (strcmp(all_data[loop].key, key) == 0) {
+            place = loop;
+            break;
         }
-        all_data[place] = append;
-        return place;
+    }
+    all_data[place] = append;
+    return place;
 }
 
 static loff_t scan_for_end(void) {
-     // look for blank spot
-                    // check sizes using the read size and if not big enough, skip
-                    // when deleting, I kept the lenghts but deleted the data
-                    uint64_t key_len;
-                    uint64_t value_len;
-                    loff_t readPos = 0;
-                    while(true) {
-                        printk(KERN_INFO "%d\n", (int)readPos);
-                        loff_t beginning = readPos;
-                        int ret = kernel_read(filp, &key_len, 8, &readPos);
-                        ret = kernel_read(filp, &value_len, 8, &readPos);
-                        if (key_len == 0 && value_len == 0) {
-                            readPos -= 16;
-                            break;
-                        }
-                       // ret = kernel_read(filp, curr_value, value_len, &write_pos);
-                        readPos += key_len+value_len;
-                    }
-                    return readPos;
+    // look for blank spot
+    // check sizes using the read size and if not big enough, skip
+    // when deleting, I kept the lenghts but deleted the data
+    uint64_t key_len;
+    uint64_t value_len;
+    loff_t readPos = 0;
+    while(true) {
+        printk(KERN_INFO "%d\n", (int)readPos);
+        loff_t beginning = readPos;
+        int ret = kernel_read(filp, &key_len, 8, &readPos);
+        ret = kernel_read(filp, &value_len, 8, &readPos);
+        if (key_len == 0 && value_len == 0) {
+            readPos -= 16;
+            break;
+        }
+        // ret = kernel_read(filp, curr_value, value_len, &write_pos);
+        readPos += key_len+value_len;
+    }
+    return readPos;
 }
 
 static int look_for_blanks(int place, struct ioctl_data *data) {
-                 // look for blank spot
-                    // check sizes using the read size and if not big enough, skip
-                    // when deleting, I kept the lenghts but deleted the data
-                    uint64_t key_len;
-                    uint64_t value_len;
-                    loff_t readPos = 0;
-                    int ret = 0;
-                    while(readPos < end_pos) {
-                        leftover = 0;
-                        printk(KERN_INFO "%d\n", (int)readPos);
-                        loff_t beginning = readPos;
-                        ret = kernel_read(filp, &key_len, 8, &readPos);
-                        ret = kernel_read(filp, &value_len, 8, &readPos);
-                        if ((key_len+value_len) < (data->key_len+data->value_len)) {
-                            readPos += key_len+value_len;
-                            continue;
-                        } else if ((key_len+value_len) - 17 > (data->key_len+data->value_len)) {
-                            // good w/ chunk
-                            leftover = (key_len+value_len) - (data->key_len+data->value_len);
-                        }  else if ((key_len+value_len) == (data->key_len+data->value_len)) {
-                            // good perfectly
-                            // do nothing
-                        }   else {
-                            // bad
-                            readPos += key_len+value_len;
-                            continue;
-                        }
-                        char *curr_key = kmalloc(1, GFP_USER);
-                        ret = kernel_read(filp, curr_key, 1, &readPos);
-                        readPos += key_len+value_len-1;
-                        //char *zeros = kmalloc(key_len+1, GFP_USER);
-                       // memset(zeros, '0', key_len);
-                        //zeros[key_len] = 0;
-                        if (curr_key[0] == 0) {
-                            write_pos = beginning; // or something? make a string of zeros and if our key is that then we got a writing place
-                            break;
-                        }
-                       // ret = kernel_read(filp, curr_value, value_len, &write_pos);
-                    }
-                    return ret;
+    // look for blank spot
+    // check sizes using the read size and if not big enough, skip
+    // when deleting, I kept the lenghts but deleted the data
+    uint64_t key_len;
+    uint64_t value_len;
+    loff_t readPos = 0;
+    int ret = 0;
+    while(readPos < end_pos) {
+        leftover = 0;
+        printk(KERN_INFO "%d\n", (int)readPos);
+        loff_t beginning = readPos;
+        ret = kernel_read(filp, &key_len, 8, &readPos);
+        ret = kernel_read(filp, &value_len, 8, &readPos);
+        if ((key_len+value_len) < (data->key_len+data->value_len)) {
+            readPos += key_len+value_len;
+            continue;
+        } else if ((key_len+value_len) - 17 > (data->key_len+data->value_len)) {
+            // good w/ chunk
+            leftover = (key_len+value_len) - (data->key_len+data->value_len);
+        }  else if ((key_len+value_len) == (data->key_len+data->value_len)) {
+            // good perfectly
+            // do nothing
+        }   else {
+            // bad
+            readPos += key_len+value_len;
+            continue;
+        }
+        char *curr_key = kmalloc(1, GFP_USER);
+        ret = kernel_read(filp, curr_key, 1, &readPos);
+        readPos += key_len+value_len-1;
+        //char *zeros = kmalloc(key_len+1, GFP_USER);
+        // memset(zeros, '0', key_len);
+        //zeros[key_len] = 0;
+        if (curr_key[0] == 0) {
+            write_pos = beginning; // or something? make a string of zeros and if our key is that then we got a writing place
+            break;
+        }
+        // ret = kernel_read(filp, curr_value, value_len, &write_pos);
+    }
+    return ret;
 }
 
 static void write_file(int place) {
     if (filp) {
-                printk(KERN_INFO, "success\n");
-                int size = all_data[place].key_len + all_data[place].value_len + sizeof(uint64_t)*2;
-                char* appendFile = kmalloc(size, GFP_USER);
-                //appendFile[0] = 0;
-                memcpy(appendFile, &all_data[place].key_len, 8);
-                memcpy(appendFile+sizeof(uint64_t), &all_data[place].value_len, 8);
-                memcpy(appendFile+2*sizeof(uint64_t), all_data[place].key, all_data[place].key_len);
-                memcpy(appendFile+2*sizeof(uint64_t)+all_data[place].key_len, all_data[place].value, all_data[place].value_len);
-                printk(KERN_INFO, "here, %s", appendFile);
-                //strcat(append, " ");
-                //strcat(append, all_data[place].value); // memcopy instead
-               // char* append = (all_data[place].key + " " + all_data[place].value);
-                void* key_mem_addr = NULL;
-                key_mem_addr = appendFile;
-             //   void* mem_addr = all_data[place].value;
-                if (write_pos != end_pos) {
-                    kernel_write(filp, key_mem_addr, size, &write_pos);
-                    if (leftover != 0) {
-                        char *chunk = kmalloc(17, GFP_USER);
-                        uint64_t zero = 0;
-                        memcpy(chunk, &leftover, 8);
-                        memcpy(chunk+8, &zero, 8);
-                        memcpy(chunk+16, &zero, 1);
-                        void* chunk_mem_addr = chunk;
-                        kernel_write(filp, chunk_mem_addr, 17, &write_pos);
-                    }
-                } else {
-                    kernel_write(filp, key_mem_addr, size, &end_pos);
-                }
-                //pos = pos+(size+1);
+        printk(KERN_INFO, "success\n");
+        int size = all_data[place].key_len + all_data[place].value_len + sizeof(uint64_t)*2;
+        char* appendFile = kmalloc(size, GFP_USER);
+        //appendFile[0] = 0;
+        memcpy(appendFile, &all_data[place].key_len, 8);
+        memcpy(appendFile+sizeof(uint64_t), &all_data[place].value_len, 8);
+        memcpy(appendFile+2*sizeof(uint64_t), all_data[place].key, all_data[place].key_len);
+        memcpy(appendFile+2*sizeof(uint64_t)+all_data[place].key_len, all_data[place].value, all_data[place].value_len);
+        printk(KERN_INFO, "here, %s", appendFile);
+        //strcat(append, " ");
+        //strcat(append, all_data[place].value); // memcopy instead
+        // char* append = (all_data[place].key + " " + all_data[place].value);
+        void* key_mem_addr = NULL;
+        key_mem_addr = appendFile;
+        //   void* mem_addr = all_data[place].value;
+        if (write_pos != end_pos) {
+            kernel_write(filp, key_mem_addr, size, &write_pos);
+            if (leftover != 0) {
+                char *chunk = kmalloc(17, GFP_USER);
+                uint64_t zero = 0;
+                memcpy(chunk, &leftover, 8);
+                memcpy(chunk+8, &zero, 8);
+                memcpy(chunk+16, &zero, 1);
+                void* chunk_mem_addr = chunk;
+                kernel_write(filp, chunk_mem_addr, 17, &write_pos);
+            }
+        } else {
+            kernel_write(filp, key_mem_addr, size, &end_pos);
+        }
+        //pos = pos+(size+1);
     }
 }
 
 static int read_array(struct ioctl_data *data, char *key) {
-            int loop = 0;
-            for(loop = 0; loop < tracker; loop++) {
-                printk(KERN_INFO "key %d of %d key %s\n", loop, tracker, all_data[loop].key);
-                if (all_data[loop].key == NULL) {
-                    continue;
-                }
-                if(strcmp(all_data[loop].key, key) == 0) {
-                    printk(KERN_INFO "found key... retrieving\n");
-                    //int ret_l =0;
-                    int amountToCopy = all_data[loop].value_len;
-                    // if want < have... copy = want
-                    if (data->value_len < all_data[loop].value_len) {
-                        amountToCopy = data->value_len;
-                    }
-                    // set return to what we have
-                    printk(KERN_INFO "%d\n", amountToCopy);
-                    data->value_len = all_data[loop].value_len;
-                 //   if (copy_to_user((void __user*)arg, data, 32)) {
-                   //         return -EFAULT;
-                    //}
-                    //printk(KERN_INFO "%s\n", all_data[loop].value);
-                   // if (copy_to_user(data.value, all_data[loop].value, amountToCopy)) {
-                     //   return -EFAULT;
-                   // }
-                    //break;
-                }
+    int loop = 0;
+    for(loop = 0; loop < tracker; loop++) {
+        printk(KERN_INFO "key %d of %d key %s\n", loop, tracker, all_data[loop].key);
+        if (all_data[loop].key == NULL) {
+            continue;
+        }
+        if(strcmp(all_data[loop].key, key) == 0) {
+            printk(KERN_INFO "found key... retrieving\n");
+            //int ret_l =0;
+            int amountToCopy = all_data[loop].value_len;
+            // if want < have... copy = want
+            if (data->value_len < all_data[loop].value_len) {
+                amountToCopy = data->value_len;
             }
-            return 0;
+            // set return to what we have
+            printk(KERN_INFO "%d\n", amountToCopy);
+            data->value_len = all_data[loop].value_len;
+            //   if (copy_to_user((void __user*)arg, data, 32)) {
+            //         return -EFAULT;
+            //}
+            //printk(KERN_INFO "%s\n", all_data[loop].value);
+            // if (copy_to_user(data.value, all_data[loop].value, amountToCopy)) {
+            //   return -EFAULT;
+            // }
+            //break;
+        }
+    }
+    return 0;
 }
 
 static int read_file(struct ioctl_data *data, char *key) {
@@ -241,7 +241,7 @@ static int read_file(struct ioctl_data *data, char *key) {
             return 0;
         }
         return -ENOENT;
-                //pos = pos+(size+1);
+        //pos = pos+(size+1);
     }
     return -EBADF;
 }
@@ -305,7 +305,7 @@ static int my_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
             write_file(place);
             // use filp
             //char data[data.key_len+data.value_len+1] = key + " " + value;
-            
+
             break;
         case 0x0001:
             if (copy_from_user(&data, (void __user*)arg, 32)) {
@@ -327,7 +327,7 @@ static int my_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
             if (ret < 0) {
                 return ret;
             }
-           // void *data_addr = data;
+            // void *data_addr = data;
             if (copy_to_user((void __user*)arg, &data, 32)) {
                 return -EFAULT;
             }
@@ -389,44 +389,44 @@ static int my_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
                 kernel_write(filp, key_mem_addr, size, &readPos);
                 return 0;
             }
-        // implement get
-        // add duplicate checking
+            // implement get
+            // add duplicate checking
 
-        // need to free data here!!!!
-        // use access_ok
+            // need to free data here!!!!
+            // use access_ok
 
-        // use cmd to dilineate btwn put/get using cmd #
+            // use cmd to dilineate btwn put/get using cmd #
     }
     return 0;
 }
 
 static int my_open(struct inode *inode, struct file *file)
 {
-	return 0;
+    return 0;
 }
 
 static int my_release(struct inode *inode, struct file *file)
 {
-	return 0;
+    return 0;
 }
 
 //populate data struct for file operations
 static const struct file_operations my_fops = {
-	.owner = THIS_MODULE,
-	.open	= my_open,
-	//.read 	= lfs_read_file,
-	//.write  = lfs_write_file,
-   // .open = &my_open,
-	.release = &my_release,
-	.unlocked_ioctl = (void*)&my_ioctl,
-	.compat_ioctl = (void*)&my_ioctl
+    .owner = THIS_MODULE,
+    .open = my_open,
+    // .read = lfs_read_file,
+    // .write = lfs_write_file,
+    // .open = &my_open,
+    .release = &my_release,
+    .unlocked_ioctl = (void*)&my_ioctl,
+    .compat_ioctl = (void*)&my_ioctl
 };
 
 //populate miscdevice data structure
 static struct miscdevice my_device = {
-	MISC_DYNAMIC_MINOR,
-	"ls3",
-	&my_fops,
+    MISC_DYNAMIC_MINOR,
+    "ls3",
+    &my_fops,
     .mode = S_IRWXUGO
 };
 
@@ -462,7 +462,7 @@ static int __init main(void) {
         filp = NULL;
         return err;
     }
-   
+
     // TODO: register as a filesystem
     // err = register_filesystem(&lfs_type);
 
@@ -486,5 +486,5 @@ static void __exit cleanup(void)
 
 // need to get user space -> kernel space pointer for laat ioctl arg
 // safeusercopy, usercopy, kernelcopy
-module_init(main)
+    module_init(main)
 module_exit(cleanup)
